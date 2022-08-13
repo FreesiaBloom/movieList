e588ef77f3bb8ca78382aa4330c94bb260de734e
FreesiaBloom <agata.jasinska215@gmail.com>
2022-08-13 10:24:59 +0200

    add header

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,17 +6,24 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
 import { MovieListComponent } from './components/movie-list/movie-list.component';
+import { HeaderComponent } from './components/common/header/header.component';
+
+// Angular Material
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MovieListComponent
+    MovieListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    StoreModule.forRoot({}, {})
+    StoreModule.forRoot({}, {}),
+    // Angular Material
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
