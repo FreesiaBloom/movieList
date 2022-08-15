c71bca90b569c3a7d03843f424ee05b0e7c900d8
FreesiaBloom <agata.jasinska215@gmail.com>
2022-08-15 18:50:54 +0200

    setuo unti test enviroment

--- frontend/src/app/directives/tag.directive.spec.ts
+++ frontend/src/app/directives/tag.directive.spec.ts
@@ -1,8 +1,28 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MovieListComponent } from '../components/content/movie-list/movie-list.component';
 import { TagDirective } from './tag.directive';
+import { RouterTestingModule } from "@angular/router/testing";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { provideMockStore } from '@ngrx/store/testing';
 
 describe('TagDirective', () => {
-  it('should create an instance', () => {
-    const directive = new TagDirective();
-    expect(directive).toBeTruthy();
+  let fixture: ComponentFixture<MovieListComponent>;
+
+  beforeEach(() => {
+    fixture = TestBed.configureTestingModule({
+      declarations: [ MovieListComponent, TagDirective ],
+      schemas:      [ CUSTOM_ELEMENTS_SCHEMA ],
+      imports:      [ RouterTestingModule, HttpClientTestingModule ],
+      providers:    [ provideMockStore({}) ]
+    })
+    .createComponent(MovieListComponent);
+    fixture.detectChanges(); // initial binding
+  });
+
+  it('should have #e0e0e0 <span>', () => {
+    const span: HTMLElement = fixture.nativeElement.querySelector('span');
+    const bgColor = span.style.backgroundColor;
+    expect(bgColor).toBe('#e0e0e0');
   });
 });
