23a5563d7357ab08d0f0e82f9528375e22106834
FreesiaBloom <agata.jasinska215@gmail.com>
2022-08-15 20:07:17 +0200

    create tests for movie service

--- frontend/src/app/services/MovieService/movie-service.spec.ts
+++ frontend/src/app/services/MovieService/movie-service.spec.ts
@@ -1,22 +1,63 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from "@angular/router/testing";
 import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { of } from 'rxjs';
 
 import { MovieService } from './movie-service';
+import { HttpClient } from '@angular/common/http';
+import { movieListTestData, movieTestData } from 'src/assets/movie-test-data';
 
 describe('MovieService', () => {
-  let service: MovieService;
+  let movieService: MovieService;
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
         HttpClientTestingModule
       ],
+      providers: [MovieService]
     });
-    service = TestBed.inject(MovieService);
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    movieService = new MovieService(httpClientSpy);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(movieService).toBeTruthy();
+  });
+
+  it('should return expected all movies list (HttpClient called once)', (done: DoneFn) => {
+    httpClientSpy.get.and.returnValue(of(movieListTestData));
+
+    movieService.getMovieList().subscribe({
+      next: movies => {
+        expect(movies)
+        .withContext('expected movielist')
+        .toEqual(movieListTestData);
+        done();
+      },
+      error: done.fail
+    });
+
+    expect(httpClientSpy.get.calls.count())
+      .withContext('one call')
+      .toBe(1);
+  });
+
+  it('should return expected movie by id (HttpClient called once)', (done: DoneFn) => {
+    httpClientSpy.get.and.returnValue(of(movieListTestData));
+
+    movieService.getMovieById(1).subscribe({
+      next: movies => {
+        expect(movies)
+        .withContext('expected movielist')
+        .toEqual(movieTestData);
+        done();
+      },
+      error: done.fail
+    });
+
+    expect(httpClientSpy.get.calls.count())
+      .withContext('one call')
+      .toBe(1);
   });
 });
