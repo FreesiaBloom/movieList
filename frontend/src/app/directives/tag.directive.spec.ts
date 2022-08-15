e29a7cc82104140db7d99a8378a36c6a4682d636
FreesiaBloom <agata.jasinska215@gmail.com>
2022-08-15 19:17:45 +0200

    add tag directive test

--- frontend/src/app/directives/tag.directive.spec.ts
+++ frontend/src/app/directives/tag.directive.spec.ts
@@ -1,28 +1,32 @@
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { MovieListComponent } from '../components/content/movie-list/movie-list.component';
 import { TagDirective } from './tag.directive';
 import { RouterTestingModule } from "@angular/router/testing";
 import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { provideMockStore } from '@ngrx/store/testing';
 
+@Component({
+  template: `<span appTag>test</span>`
+})
+class TestTagDirectiveComponent { }
+
 describe('TagDirective', () => {
-  let fixture: ComponentFixture<MovieListComponent>;
+  let fixture: ComponentFixture<TestTagDirectiveComponent>;
 
   beforeEach(() => {
     fixture = TestBed.configureTestingModule({
-      declarations: [ MovieListComponent, TagDirective ],
+      declarations: [ TestTagDirectiveComponent, TagDirective ],
       schemas:      [ CUSTOM_ELEMENTS_SCHEMA ],
       imports:      [ RouterTestingModule, HttpClientTestingModule ],
       providers:    [ provideMockStore({}) ]
     })
-    .createComponent(MovieListComponent);
+    .createComponent(TestTagDirectiveComponent);
     fixture.detectChanges(); // initial binding
   });
 
-  it('should have #e0e0e0 <span>', () => {
+  it('should have grey background <span>', () => {
     const span: HTMLElement = fixture.nativeElement.querySelector('span');
     const bgColor = span.style.backgroundColor;
-    expect(bgColor).toBe('#e0e0e0');
+    expect(bgColor).toBe('rgb(224, 224, 224)');
   });
 });
